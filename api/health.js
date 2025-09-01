87cff2a28b6e006da9149f2bc00ec399cca3fb4b
pturgeon06-hub <pturgeon06@hotmail.com>
2025-09-01 09:06:45 -0400

    Delete api directory

--- api/health.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ ok: false, error: 'Method not allowed' });
-  return res.status(200).json({ ok: true });
-}
